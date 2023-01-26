c78cdb3b716115d7af07f0cb06bbcab9c9215271
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-01-25 20:51:17 -0500

    Update Labora5.py

--- Labora5.py
+++ Labora5.py
@@ -3,3 +3,32 @@ class Animal:
         self.nom = nom
         self.type = type
 
+menu = {1 : "Viande",
+        2: "Plante",
+        3: "Viande et Plante"}
+
+Dragon = (1, "Dragon", "Carnivore")
+Cheval = (2, "Dragon", "Carnivore")
+Panda = (3, "Dragon", "Carnivore")
+Wapiti = (3, "Dragon", "Carnivore")
+Gnous = (2, "Dragon", "Carnivore")
+Lynx = (1, "Dragon", "Carnivore")
+Ours = (3, "Dragon", "Carnivore")
+Bison = (2, "Dragon", "Carnivore")
+Loup = (1, "Dragon", "Carnivore")
+Cochon = (3, "Dragon", "Carnivore")
+
+animaux_liste = [Dragon, Cheval, Panda, Wapiti, Gnous, Lynx, Ours, Bison, Loup, Cochon]
+
+def Question():
+    while True:
+        print("Choisez le bon type d'animaux dans la liste")
+        print("Voir ce que mange tous les animaux dans la liste")
+        réponse_utilisateur = print(int("Choisisez une question: "))
+        if réponse_utilisateur == 1:
+            pass
+        elif réponse_utilisateur == 2:
+            pass
+        else:
+            print("Votre réponse est mauvaise, choisisez une autre réponse")
+            False
